24a227f4a1d1dbfc6d3e6b8247625663211cfb87
Georgy Vashchenko <gvashchenko@lineate.com>
2021-12-03 00:30:09 +0600

    Init project
    Add 2 modules: service1, service2

---
+++ modules/service2/src/main/java/com/lineate/internal/idp/interaction/service2/Service2Application.java
@@ -0,0 +1,12 @@
+package com.lineate.internal.idp.interaction.service2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Service2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Service2Application.class, args);
+    }
+}
